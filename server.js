ad88b798231eede0c5d4bf1d2d6d24f56b8957a5
Matias97788 <matiasrodriguezsandoval@outlook.com>
2023-08-18 16:55:09 -0400

    update

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const crypto = require("crypto");
+const nodemailer = require("nodemailer");
 
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("¡Hola desde la API!");
-  
 });
+
 app.post("/webhook/pedidos-pagados", (req, res) => {
   const hmacHeader = req.get("X-Shopify-Hmac-Sha256");
   const webhookSecret =
@@ -23,12 +24,21 @@ app.post("/webhook/pedidos-pagados", (req, res) => {
     // La firma HMAC coincide, el webhook es auténtico
     const payload = req.body;
 
-    // Imprimir todos los valores enviados por el webhook
-    console.log("Valores enviados por el webhook:");
-    console.log(payload);
+    // Almacenar los valores enviados por el webhook en una variable
+    const payloadString = JSON.stringify(payload);
 
     // Responde a Shopify para confirmar la recepción del webhook
     res.sendStatus(200);
+
+    // Imprimir el payload en la consola
+    console.log("Valores enviados por el webhook:");
+    console.log(payload);
+
+    // Enviar notificación por correo electrónico
+    enviarNotificacion(payload);
+
+    // Retornar el payload como respuesta
+    return payloadString;
   } else {
     // El webhook no es auténtico, responde con un error
     res.sendStatus(401);
@@ -36,7 +46,36 @@ app.post("/webhook/pedidos-pagados", (req, res) => {
 });
 
 const port = process.env.PORT || 80;
-
 app.listen(port, () => {
   console.log(`Servidor Express escuchando en el puerto ${port}`);
 });
+
+async function enviarNotificacion(payload) {
+  try {
+    // Configurar el transporte de correo electrónico
+    const transporter = nodemailer.createTransport({
+      // Aquí debes configurar tus datos de servidor de correo saliente (SMTP)
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: true,
+      auth: {
+        user: "matigoldercazar@gmail.com",
+        pass: "matigolacdc",
+      },
+    });
+
+    // Configurar el contenido del correo electrónico
+    const mensaje = {
+      from: "noreply@example.com",
+      to: "m.rodriguez@agencialosnavegantes.cl",
+      subject: "Notificación de Webhook",
+      text: JSON.stringify(payload),
+    };
+
+    // Enviar el correo electrónico
+    const info = await transporter.sendMail(mensaje);
+    console.log("Correo electrónico enviado:", info.response);
+  } catch (error) {
+    console.error("Error al enviar el correo electrónico:", error);
+  }
+}
